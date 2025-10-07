84ff1dc130331789d56be45d3d31f80ae9ba042b
Joseph Cho <ujocho0913@gmail.com>
2025-10-07 13:21:30 -0500

    activate pages in st.nav

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,19 +8,21 @@ from pathlib import Path
 jcpao_logo = Path("assets/logo/jcpao_logo_750x750.png")
 
 st.set_page_config(
-    page_title="JCPAO Portal", # JCPAO staff-view
+    page_title="JCPAO Dashboard", # JCPAO staff-view
     page_icon=jcpao_logo, 
     layout="wide", # "centered" or "wide"
     initial_sidebar_state="expanded",
     menu_items={
         # 'Get Help': 'https://www.extremelycoolapp.com/help',
         'Report a bug': "mailto:ujcho@jacksongov.org", # To report a bug, please email
-        'About': "The JCPAO Dashboard was built by Joseph Cho and the Crime Strategies Unit (CSU) of the Jackson County Prosecuting Attorney's Office."
+        'About': "The JCPAO Dashboard was developed by Joseph Cho (Crime Analyst) and the Crime Strategies Unit (CSU) of the Jackson County Prosecuting Attorney's Office. For questions, suggestions, or bugs, please reach out to Joseph Cho at ujcho@jacksongov.org!"
     }
 )
 
 # --- JCPAO Streamlit page logo --- 
 st.logo(jcpao_logo, size="large", link="https://www.jacksoncountyprosecutor.com")
+
+# --- Steamlit sidebar --- 
 
 
 # --- Page Navigation ---
@@ -35,9 +37,10 @@ pages = {
     # "Violent Crime in KCMO": [
     #     st.Page("pages/violence_pages/shoot_review.py", title="Overview")
     # ],
-    # "Domestic Violence": [
-    #     st.Page("pages/dv_pages/dv_main.py", title="Overview")
-    # ],
+    "Domestic Violence": [
+        st.Page("pages/dv_pages/dv_main.py", title="Overview"),
+        st.Page("pages/dv_pages/dv_cases.py", title="Domestic Assault Cases")
+    ],
     # "Other": [
     #     st.Page("pages/other_pages/blairs_law.py", title="Blair's Law"),
     #     st.Page("pages/other_pages/valentines_law.py", title="Valentine's Law")
@@ -45,6 +48,7 @@ pages = {
     "Resources": [
         st.Page("pages/resources_pages/about_jcpao.py", title="About the JCPAO"),
         st.Page("pages/resources_pages/learn_more.py", title="Process of a Case"),
+        st.Page("pages/resources_pages/codes_glossary.py", title="Criminal Charge Codes")
         # st.page_link("https://jcpao-search.streamlit.app/", label="Police Report Search Tool", icon="🔎"),
         # st.page_link("https://jacksoncountyprosecutor.com", label="JCPAO Website")
     ],
