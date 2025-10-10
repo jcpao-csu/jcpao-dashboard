7918a3401653f6660444ea6f3a1308bff435866a
Joseph Cho <ujocho0913@gmail.com>
2025-10-10 09:32:41 -0500

    add filters to codes glosssary

--- pages/resources_pages/codes_glossary.py
+++ pages/resources_pages/codes_glossary.py
@@ -16,15 +16,27 @@ st.divider()
 # Define update_df() function
 def update_df():
 
-    st.session_state["codes_glossary"] = MSHP_CODEBOOK
+    # st.session_state["codes_glossary"] = MSHP_CODEBOOK
+    df = MSHP_CODEBOOK
 
     # Filter by selected MSHP Charge Code Category
     if st.session_state["charge_category_filter"]:
         # col_names = [col_name.upper() if col_name.upper()=="ESCAPE" else col_name.lower().replace(" ", "_") for col_name in st.session_state["charge_category_filter"]]
     
-        df = st.session_state["codes_glossary"]
+        # df = st.session_state["codes_glossary"]
         df = df.loc[df["JCPAO Category"].isin(st.session_state["charge_category_filter"])].copy().reset_index(drop=True)
 
+    # Filter by legacy charge code 
+    if st.session_state["legacy_code_filter"]:
+        df = df.loc[df["Legacy Code"]].copy().reset_index(drop=True)
+
+    # Filter by charge severity
+    if st.session_state["severity_filter"] != "All":
+        if st.session_state["severity_filter"] == "O": # Other
+            df = df[df["Severity"].isin(["A", "U"])].copy().reset_index(drop=True)
+        else:
+            df = df[df["Severity"]==st.session_state["severity_filter"]].reset_index(drop=True)
+    
         # Reassign to session state
         st.session_state["codes_glossary"] = df
 
@@ -32,6 +44,8 @@ def update_df():
 def reset_filters():
     st.session_state["charge_category_filter"] = [] # MSHP Charge Code Categories
     st.session_state["codes_glossary"] = MSHP_CODEBOOK
+    st.session_state["legacy_code_filter"] = False
+    st.session_state["severity_filter"] = "All"
 
 # Create widget filter
 def initiate_widgets(disabled: bool = False, color: str = "violet"):
@@ -43,12 +57,46 @@ def initiate_widgets(disabled: bool = False, color: str = "violet"):
         default=[],
         # format_func=None,
         key="charge_category_filter",
-        help="Select the charge code categories of the data you would like to examine through the dashboard.",
+        help="Select (up to five) charge code categories.",
         on_change=update_df,
         max_selections=5,
         placeholder="All",
         disabled=disabled,
         label_visibility="visible",
+        accept_new_options=False,
+        width="stretch"
+    )
+
+    # filter by active charge code 
+    filter_legacy = st.toggle(
+        f":{color}-background[:{color}[**Active Charge Codes Only**] 🚔]",
+        value=False,
+        key="legacy_code_filter",
+        help="Filter charge codes to only view ones that are currently active.",
+        on_change=update_df,
+        disabled=disabled,
+        width="stretch"
+    )
+
+    # filter by charge code severity
+    sev_dict = {
+        "All": "All Charge Codes",
+        "F": "Felony",
+        "M": "Misdemeanor",
+        "I": "Infraction",
+        "L": "Ordinance",
+        "O": "Other" # A or U
+    }
+    filter_sev = st.selectbox(
+        f":{color}-background[:{color}[**Charge Code Severity**] ⚖️]",
+        options=sev_dict.keys(),
+        index=0,
+        format_func=lambda x: sev_dict[x],
+        key="severity_filter",
+        help="Filter charge codes by severity.",
+        on_change=update_df,
+        placeholder="Select charge severity to filter",
+        disabled=disabled,
         accept_new_options=False,
         width="stretch"
     )
@@ -80,7 +128,7 @@ def initiate_widgets(disabled: bool = False, color: str = "violet"):
     ) # https://www.mshp.dps.missouri.gov/CJ08Client/Home/ChargeCode
 
     # Produce variables
-    return filter_category
+    return filter_category, filter_legacy, filter_sev
 
 
 # --- st.sidebar ---
